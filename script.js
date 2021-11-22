ee31af33bb7aaf8c7197bb406d3937deb60843cb
Ohmcode <email@jasperfu.com>
2021-11-22 17:49:42 -0500

    console code done, play round and play again added

--- script.js
+++ script.js
@@ -2,24 +2,73 @@
 //played from console 
 //Jasper 11/22/21
 
+const choices = ['rock', 'paper', 'scissors']
+let playerplay = "";
+let computerplay = "";
+
+playRound()
+
+
+
 
 function computerPlay(){
-    //mathrandom integer number
-    //round up to integer
+
+    let randomNumber = Math.floor(Math.random()*3); //mathrandom integer number
+    return choices[randomNumber]
     //return rock , paper or scissors based on int
-
 }
 
-let playerSelection = prompt("Will you choose rock, paper, or scissors?","").toLowerCase;//define playerSelection as string prompt input to lowercase
+function playAgain(){
+    confirm("Play again?")? playRound():alert("thanks for playing!"); 
+
+}  
+
+function playRound(){
+    playerplay =prompt("Will you choose rock, paper, or scissors?","").toLowerCase();
+    computerplay = computerPlay();
+    playerVScomputerString = playerplay + computerplay;
+    //debug code
+    console.log(playerplay)
+    console.log(computerplay)
+//end debug code
 
 
-function playRound(playerSelection, computerSelection){
-
-    //playerSelection and computerSelection same, then tie
-    //switch case player rock, computer scissors or paper 
-    // player paper, cp scissors or rock
-    //player scissors, cp rock or paper
-    // 
-
+    if (playerplay != "rock" && playerplay != "paper" && playerplay != "scissors"){
+        alert("invalid play");
+        playAgain()
+        return;
+    }
+    if (playerplay === computerplay){
+        alert("tie");
+        playAgain()
+        return;
+    }
+    switch(playerVScomputerString){
+        case 'rockpaper':
+            alert("paper beats rock, you lose :(")
+            break;
+        case 'rockscissors':
+            alert("rock beats scissors! WINNER")  
+            break;  
+        case 'paperscissors':  
+            alert("scissors beat paper, maybe you should cut your losses")   
+            break;
+        case 'paperrock':  
+            alert("paper beats rock! that's a wrap") 
+            break;
+        case 'scissorspaper':
+            alert("scissors beats paper, you really showed the computer")
+            break;
+        case 'scissorsrock':  
+            alert("ouch, your scissors was beat up by a rock. better luck next time!")
+            break;
+        default:
+            alert("you must play rock, paper or scissors")
+        break;    
+    }
+    playAgain();
 }
+    
+
+
 
