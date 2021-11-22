f429ba2a0818d90d4ed1644677f20a345c8377b6
Ohmcode <email@jasperfu.com>
2021-11-22 16:01:39 -0500

    initial files

---
+++ script.js
@@ -0,0 +1,25 @@
+//rock paper scissors game
+//played from console 
+//Jasper 11/22/21
+
+
+function computerPlay(){
+    //mathrandom integer number
+    //round up to integer
+    //return rock , paper or scissors based on int
+
+}
+
+let playerSelection = prompt("Will you choose rock, paper, or scissors?","").toLowerCase;//define playerSelection as string prompt input to lowercase
+
+
+function playRound(playerSelection, computerSelection){
+
+    //playerSelection and computerSelection same, then tie
+    //switch case player rock, computer scissors or paper 
+    // player paper, cp scissors or rock
+    //player scissors, cp rock or paper
+    // 
+
+}
+
